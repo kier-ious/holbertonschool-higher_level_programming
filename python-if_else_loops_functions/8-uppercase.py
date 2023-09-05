4c8fa15e703ff114cf821a93cf0b0dab8f1534a1
kier-ious <kier.diemer@gmail.com>
2023-09-05 15:31:00 -0700

    task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    numcheck = ord(c)
-    if numcheck >= 65 and numcheck <= 91:
-        return True
-    else:
-        return False
+def uppercase(str):
+    for i in str:
+        if 97 <= ord(i) < 123:
+            i = chr(ord(i) - (97 - 65))
+        print("{}".format(i), end="")
+    print(" ")
