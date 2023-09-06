e5b1e4fa4029a025c65919e3b065597b9b66861f
kier-ious <kier.diemer@gmail.com>
2023-09-06 12:46:14 -0700

    .

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == "__main__":
     add = 0
     for i in range(1, len(sys.argv)):
-        add += int(sys.argv[1])
+        add += int(sys.argv[i])
     print("{:d}".format(add))
