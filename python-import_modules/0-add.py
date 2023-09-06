0ec866445f1404a6c948cadd785fb44a728cedfa
kier-ious <kier.diemer@gmail.com>
2023-09-06 10:57:37 -0700

    .

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
+def main():
     a = 1
     b = 1
     print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+
+    if __name__ == "__main__":
+    	main()
