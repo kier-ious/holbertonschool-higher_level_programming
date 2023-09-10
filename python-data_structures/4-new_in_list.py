828d5e5b8383103de4cdb63b4fb388f216c09a64
kier-ious <kier.diemer@gmail.com>
2023-09-10 16:30:39 -0700

    task4

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+
+    new_in_list = my_list[:]
+
+    if 0 <= idx <length:
+        new_in_list[idx] = element
+
+    return(new_in_list)
