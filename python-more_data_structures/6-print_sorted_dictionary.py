da1d5d199182b0cb37fd541b546b5b2f98e7a26f
kier-ious <kier.diemer@gmail.com>
2023-09-11 14:22:38 -0700

    task6 fixed print statement

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    my_keys = list(a_dictionary.keys())
-    sorted_keys = my_keys.sort()
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        print(f"{key}: {a_dictionary[key]}")
