3f03468e14b46926fe5eb62cb04db66e483bc5b2
kier-ious <kier.diemer@gmail.com>
2023-09-11 14:14:25 -0700

    task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]):
-        print("{} : {}".format(key, value))
+    my_keys = list(a_dictionary.keys())
+    sorted_keys = my_keys.sort()
