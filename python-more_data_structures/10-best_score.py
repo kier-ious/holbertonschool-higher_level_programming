27a2ecef5089ed69d3ac4a918993932386494e53
kier-ious <kier.diemer@gmail.com>
2023-09-12 12:02:45 -0700

    .

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
 
-    max_key = max(a_dictionary, key=a_dictionary.get)
-    return
+    max_key = max(a_dictionary, key=lambda k: a_dictionary[k])
+    return max_key
