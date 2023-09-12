5bad24a5bcedf60f1d398b2a456b0810c3716bdc
kier-ious <kier.diemer@gmail.com>
2023-09-12 14:28:45 -0700

    removed the last print statement

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
         print()
     except IndexError:
-        print("list index out of range")
+        print()
 
     return count
