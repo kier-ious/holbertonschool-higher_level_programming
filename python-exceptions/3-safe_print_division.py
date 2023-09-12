85fc0edd79fe564c4bf73c520c41e96ca1961254
kier-ious <kier.diemer@gmail.com>
2023-09-12 14:43:34 -0700

    took out the except line

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,11 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         print("Error: Division by zero")
-        result = None
-    except Exception as k:
-        print(f"Error: {k}")
         result = None
     finally:
         print("Inside result: {}".format(result))
