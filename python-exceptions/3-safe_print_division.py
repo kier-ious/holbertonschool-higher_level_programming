1a63a28c19af9a982cc5666eebe669f6c16ed8d8
kier-ious <kier.diemer@gmail.com>
2023-09-12 14:44:27 -0700

    .

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (ZeroDivisionError, TypeError):
+    except ZeroDivisionError:
         print("Error: Division by zero")
         result = None
     finally:
