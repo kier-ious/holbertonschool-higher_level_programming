23c560327b572dcd3a1ec4e8b072c6fe9d6d8059
kier-ious <kier.diemer@gmail.com>
2023-09-12 14:45:36 -0700

    took out another print statement

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,6 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Error: Division by zero")
         result = None
     finally:
         print("Inside result: {}".format(result))
