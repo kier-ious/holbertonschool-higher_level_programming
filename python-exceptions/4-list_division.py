6faa3ea82e40c4d562bbe5a1779ca55363cba97a
kier-ious <kier.diemer@gmail.com>
2023-09-12 16:39:44 -0700

    task 4, still need to make edits. won't run in terminal

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+
+    for i in range(list_length):
+        try:
+            piece_1 = my_list_1[i] if < len(my_list_1) else 0
+            piece_2 = my_list_2[i] if < len(my_list_2) else 0
+
+            if not isinstance(piece_1, (int, float)) or not
+            isinstance(piece_2, (int, float)):
+                raise TypeError("wrong type")
+
+            if piece_2 == 0:
+                raise (ZeroDivisionError, IndexError)("division by 0")
+
+            division_result = piece_1 / piece_2
+            result.append(division_result)
+
+        finally:
+            pass
+    return result
