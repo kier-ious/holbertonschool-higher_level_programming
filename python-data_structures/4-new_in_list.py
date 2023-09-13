0b18a863371de67ba4ed5003aa36a42375531c24
kier-ious <kier.diemer@gmail.com>
2023-09-12 17:48:49 -0700

    pycodestyle

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
 
     new_in_list = my_list[:]
 
-    if 0 <= idx <length:
+    if 0 <= idx < length:
         new_in_list[idx] = element
 
     return(new_in_list)
