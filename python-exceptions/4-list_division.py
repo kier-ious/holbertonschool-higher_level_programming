96dfbd27545cbf40628ceaa173744a4d64127e4c
kier-ious <kier.diemer@gmail.com>
2023-09-12 17:34:37 -0700

    added result_division = 0 to every except

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,15 +5,17 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             result_division = my_list_1[i] / my_list_2[i]
+
         except TypeError:
             print("wrong type")
-            result.append(0)
+            result_division = 0
         except ZeroDivisionError:
             print("division by 0")
-            result.append(0)
+            result_division = 0
         except IndexError:
             print("out of range")
-            result.append(0)
+            result_division = 0
+
         finally:
             result.append(result_division)
 
