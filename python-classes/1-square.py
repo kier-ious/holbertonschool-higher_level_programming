186b98a9a895193492f39596bff61ac044131b5c
kier-ious <kier.diemer@gmail.com>
2023-09-14 14:50:51 -0700

    task 1 includes a private instance attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Private instance attribute"""
+
+    def __init__(self, size):
+        """init Private attribute"""
+        self.size = size
+        print(self)
