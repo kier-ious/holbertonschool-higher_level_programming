cce9cdcc31ecb2878c9a4850bb0bdff743239566
kier-ious <kier.diemer@gmail.com>
2023-09-15 11:11:56 -0700

    task 4 included getters and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This class defines a Square"""
+
+
+class Square:
+    """Square class with size attribute"""
+
+    def __init__(self, size=0):
+        """init method w/ optional size"""
+        self.__size = size
+
+    def area(self):
+        """Calculate are of the Square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter for size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size in method"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
