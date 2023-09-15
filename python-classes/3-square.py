f85928a02cf9861f5e759c03378cf2de530f98c7
kier-ious <kier.diemer@gmail.com>
2023-09-15 10:34:21 -0700

    task 3 returns the current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This class defines a Square"""
+
+
+class Square:
+    """Square class with size attribute"""
+
+    def __init__(self, size=0):
+        """init method w/ optional size"""
+        self.__size = size
+
+    def area(self):
+        """Calculate are of the Square"""
+        #Access size attribute using self.__size
+        return self.__size ** 2
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
