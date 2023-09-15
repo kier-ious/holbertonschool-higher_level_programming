bf80564bba8bd3e9246e06c4ea7aed96a0f0be17
kier-ious <kier.diemer@gmail.com>
2023-09-14 17:38:56 -0700

    task 1 including self

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,9 +2,8 @@
 
 
 class Square:
-    """Private instance attribute"""
+    """Square class with size attribute"""
 
     def __init__(self, size):
         """init Private attribute"""
         self.size = size
-        print(self)
