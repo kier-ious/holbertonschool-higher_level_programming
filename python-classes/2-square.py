6c66ff29d9fdbd01325f443d108f448818b50e73
kier-ious <kier.diemer@gmail.com>
2023-09-14 18:21:50 -0700

    added raised errors

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,6 +10,6 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("Size must be an integer")
         elif size < 0:
-            rasie ValueError("size must >= 0")
+            raise ValueError("size must >= 0")
         else:
             self.__size = size
