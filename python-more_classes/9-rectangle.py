d2f13c2c0003ac7f878d537bcde006b635bbcf37
kier-ious <kier.diemer@gmail.com>
2023-09-17 15:51:13 -0700

    added more docstrings

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -71,10 +71,12 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """statis method for returning the largest rect"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise ValueError("rect_2 must be an instance of Rectangle")
+
         if rect_1.area() >= rect_2.area():
             return rect_1
         else:
