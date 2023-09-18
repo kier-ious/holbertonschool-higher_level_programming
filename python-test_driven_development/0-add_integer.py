20dadc1122b1719e01a97fa4b91f2a22b241751a
kier-ious <kier.diemer@gmail.com>
2023-09-18 13:48:35 -0700

    task 0, wrote test code and main code. Still not getting the correct output.

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b=98):
+    """Checking to see if a is an integer or a float"""
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    """Checking to see if b is an integer or a float"""
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integr")
+
+    return int(a) + int(b)
