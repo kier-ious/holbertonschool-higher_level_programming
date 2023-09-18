dc7cb86144c4589fee6200f7fdf302f2c52bc37c
kier-ious <kier.diemer@gmail.com>
2023-09-18 16:35:53 -0700

    task 1 dividing the matrix

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Dividing a matrix"""
+
+
+def matrix_divided(matrix, div):
+    the_divided_matrix = []
+
+    for row in matrix:
+        if not isinstance(row, list) or not all(isinstance(element, (int, float)) for element in row):
+            return False
+        return True
+
+    if not matrix_divided(matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
