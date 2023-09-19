e918b78a0de3692d174bfd0459e28f5bf0ce1d2f
kier-ious <kier.diemer@gmail.com>
2023-09-18 17:09:56 -0700

    fixed the syntax in the module so that the TypeErrors would print properly and I include a new_matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,10 +4,25 @@
 
 def matrix_divided(matrix, div):
     """Dives elements of the matrix by div"""
-    for row in matrix:
-        if not isinstance(row, list) or not all(isinstance(element, (int, float)) for element in row):
-            return False
-        return True
 
-    if not matrix_divided(matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    if not isinstance(matrix, list) or not all(
+        isinstance(row, list) and all(
+            isinstance(element, (int, float)) for element in row
+            ) for row in matrix
+            ):
+        raise TypeError("matrix must be a matrix (list of lists) \
+                        of integers/floats")
+
+    row_size = len(matrix[0])
+    if not all(len(row)) == row_size for row in matrix:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    new_matrix = [[round(element / div, 3) for element in row] for row in matrix]
+
+    return new_matrix
