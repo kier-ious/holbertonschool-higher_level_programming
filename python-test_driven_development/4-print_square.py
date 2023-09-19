6edf6e66c46d2f2a316a661515e78d736dbdb357
kier-ious <kier.diemer@gmail.com>
2023-09-19 13:52:50 -0700

    added docstrings to the module

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,12 +3,15 @@
 
 
 def print_square(size):
+    """Checking to see if it's an int"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
+        """CHecking to see if it's >= to 0"""
         raise ValueError("size must be >= 0")
     for o in range(size):
         for k in range(size):
             print("#", end="")
+            """Printing the square with #'s"""
 
         print()
