22236fa1263e728768c7a8275d4cb58b2f7ae095
kier-ious <kier.diemer@gmail.com>
2023-09-19 15:08:06 -0700

    added to module

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,4 +7,21 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    print()
+    """Split the text into lines"""
+    lines = text.splitlines()
+    result = []
+
+    """Iterate through each line"""
+    for line in lines:
+        """Remove trailing white space"""
+        line = line.strip()
+        if line:
+            result.append(line)
+
+    """Print the new lines with extra newlines"""
+    for i, line in enumerate(result):
+        """Adding newlines with sentences that have these special characters"""
+        if i > 0 and (line.ends(".") | ("?") | (":")):
+            print()
+
+        print(line)
