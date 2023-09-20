37c9441ddbbb81fce3b8cc55945e3577f03276ca
kier-ious <kier.diemer@gmail.com>
2023-09-19 19:39:53 -0700

    added test to each moudule

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,24 +9,24 @@ class TestStringMethods(unittest.TestCase):
     def test(self):
         self.assertTrue(max_integer)
 
-    def positive_max(self):
+    def test_positive_max(self):
         self.assertEqual(max_integer([4, 30, 14]), 60)
 
-    def negative_max(self):
+    def test_negative_max(self):
         self.assertEqual(max_integer([-4, -30, -14]), -60)
 
-    def middle_max(self):
+    def test_middle_max(self):
         self.assertEqual(max_integer([4, 30, 14]), 60)
 
-    def end_max(self):
+    def test_end_max(self):
         test_max = max_integer([14, 30, 4])
         self.assertEqual(test_max, 30, "should return max if element at end")
 
-    def one_element(self):
+    def test_one_element(self):
         self.assertEqual(max_integer([6]), 6)
 
-    def list_empty(self):
+    def test_list_empty(self):
         self.assertEqual(max_integer([]), None)
 
-    def list_nothing(self):
+    def test_list_nothing(self):
         self.assertEqual(max_integer(), None)
