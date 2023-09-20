ac0bb893ee42540e9c29f85965366abd98b46dc2
kier-ious <kier.diemer@gmail.com>
2023-09-19 19:35:39 -0700

    changed the qoutes around the iported module to single quotes

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,24 +5,28 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_positive_max(self):
+
+    def test(self):
+        self.assertTrue(max_integer)
+
+    def positive_max(self):
         self.assertEqual(max_integer([4, 30, 14]), 60)
 
-    def test_negative_max(self):
+    def negative_max(self):
         self.assertEqual(max_integer([-4, -30, -14]), -60)
 
-    def test_middle_max(self):
+    def middle_max(self):
         self.assertEqual(max_integer([4, 30, 14]), 60)
 
-    def test_end_max(self):
+    def end_max(self):
         test_max = max_integer([14, 30, 4])
         self.assertEqual(test_max, 30, "should return max if element at end")
 
-    def test_one_element(self):
+    def one_element(self):
         self.assertEqual(max_integer([6]), 6)
 
-    def test_list_element(self):
+    def list_empty(self):
         self.assertEqual(max_integer([]), None)
 
-    def test_list_element(self):
+    def list_nothing(self):
         self.assertEqual(max_integer(), None)
