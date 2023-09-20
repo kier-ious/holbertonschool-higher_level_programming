57af9365b51cbc7bb5be80a6bc9f40dfc89bdae8
kier-ious <kier.diemer@gmail.com>
2023-09-19 18:15:27 -0700

    .

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,20 +7,34 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    """Split the text into lines"""
+    """Split the text into lines
     lines = text.splitlines()
     result = []
 
-    """Iterate through each line"""
+    Iterate through each line
     for line in lines:
-        """Remove trailing white space"""
+        Remove trailing white space
         line = line.strip()
         if line:
             result.append(line)
 
-    """Print the new lines with extra newlines"""
+    Print the new lines with extra newlines
     for i, line in enumerate(result):
-        """Adding newlines with sentences that have these special characters"""
+        dding newlines with sentences that have these special characters
         if i > 0 and (line.endswith(".", "?", ":")):
             print()
-        print(line)
+        print(line)"""
+
+    """Initializing 'words'"""
+    words = text
+
+    delimiters = "?:."
+    for delimiter in "?:.":
+        """Split the words based on delim"""
+        word_list = words.split(delimiter)
+
+        """Remove leading & trailing spaces"""
+        word_list = [word.strip() for word in word_list]
+
+        """Join words back 2gether w/ delim & the newline"""
+        words = (delimiter + "\n\n").join(word_list)
