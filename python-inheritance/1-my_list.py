2a7abbbed61038e7ce720caf3a67cce16a54d239
kier-ious <kier.diemer@gmail.com>
2023-09-25 13:43:44 -0700

    added docstring in main file

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,6 +3,11 @@
 
 
 class MyList(list):
+    """Class for MyList
+
+    Args:
+        list (self): Printing sorted list
+    """
     def print_sorted(self):
         """Using the sort function to print sorted list"""
         sorted_list = sorted(self)
