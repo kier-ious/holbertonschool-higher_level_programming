eb729261f14fe05da6d56ca16dbc6b7eb090c289
kier-ious <kier.diemer@gmail.com>
2023-09-25 13:50:34 -0700

    task 3, checking if it's the same class or if it's inherited

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,3 +5,4 @@
 def is_same_class(obj, a_class):
     """Using isinstance to check if obj is in same class"""
     return isinstance(obj, a_class)
+
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Checking if the objest is an instance of the class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Using isinstance to check if obj is in same class"""
+    return isinstance(obj, a_class)
