6514cf09888ec3634cac620b110c5f35c65ee74a
kier-ious <kier.diemer@gmail.com>
2023-09-25 13:42:22 -0700

    fixed edge cases

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,5 +5,5 @@
 class MyList(list):
     def print_sorted(self):
         """Using the sort function to print sorted list"""
-        sorted_list = sorted(self, int)
+        sorted_list = sorted(self)
         print(sorted_list)
