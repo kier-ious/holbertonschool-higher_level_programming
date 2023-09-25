8410652d822490aac3b91738d7eab8989d98b267
kier-ious <kier.diemer@gmail.com>
2023-09-25 13:54:19 -0700

    added if else statements

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,5 +3,8 @@
 
 
 def is_same_class(obj, a_class):
-    """Using isinstance to check if obj is in same class"""
-    return type(obj, a_class)
+    """Using type to check if obj is in same class"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
