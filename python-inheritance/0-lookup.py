593f8011ad4f1ed405ac32b14448d97096a25a2f
kier-ious <kier.diemer@gmail.com>
2023-09-24 19:31:19 -0700

    added addition blank line under first docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""function that returns the list of available
-    attributes and methods of an object"""
+"""Lookup"""
+
 
 def lookup(obj):
     """Function that lists all available attributes & methods of
