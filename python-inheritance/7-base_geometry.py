b4b408a1833ec340aefd4bc97b7496fa36a00f19
kier-ious <kier.diemer@gmail.com>
2023-09-25 16:44:01 -0700

    task 7, integer validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Integer Validation"""
+
+
+class BaseGeometry:
+    """The empty base class"""
+    def area(self):
+        """Raise an exception error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Using integer_value to determine status of name and value
+
+        Args:
+            name (string): Public instance attribute
+            value (integer): Public instance attribute
+
+        Raises:
+            TypeError: must be an integer
+            ValueError: must be greater than 0
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
