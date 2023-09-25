cf4bc265c0cfa1cc33c9197ff60c8b2691162e54
kier-ious <kier.diemer@gmail.com>
2023-09-24 21:08:21 -0700

    added docstrings

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""_Checking if the objest is an instance of the class """
 
 
 def is_same_class(obj, a_class):
-        return isinstance(obj, a_class)
+    """Using isinstance to check if obj is in same class"""
+    return isinstance(obj, a_class)
