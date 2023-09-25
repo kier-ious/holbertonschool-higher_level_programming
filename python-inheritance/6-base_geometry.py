3553a0dfa05f2bccc3a4b404a845b7009c891b64
kier-ious <kier.diemer@gmail.com>
2023-09-25 14:53:53 -0700

    task 6, raise exception

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Improving the BaseGeometry class"""
+
+
+class BaseGeometry:
+    """The empty class"""
+
+    def area(self):
+        """Raise an exception error"""
+        raise Exception("area() is not impl")
