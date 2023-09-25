82a5c3a9eb3aca86acd793d8d2888d82d6cf147b
kier-ious <kier.diemer@gmail.com>
2023-09-25 14:07:37 -0700

    task 4, determining status of subclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Checking to see if it's a subclass of"""
+
+
+def inherits_from(obj, a_class):
+    """Using issubclass to determine if its a subclass"""
+    return issubclass(type(obj), a_class)
