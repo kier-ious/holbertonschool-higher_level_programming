c076e160b0c83e9c1ca004150c0699a5564e64b5
kier-ious <kier.diemer@gmail.com>
2023-09-24 20:40:53 -0700

    task 1, printing sorted list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Printing sort ascending list"""
+
+
+class MyList(list):
+
+
+    def print_sorted(self):
+        """Using the sort function to print sorted list"""
+        sorted_list = sorted(self)
+        print(sorted_list)
