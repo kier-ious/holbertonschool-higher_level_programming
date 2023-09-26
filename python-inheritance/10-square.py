816ad2d081e0ec2e1ee38d60e0ef23bcf42b2cf6
kier-ious <kier.diemer@gmail.com>
2023-09-26 09:49:32 -0700

    added docstrings

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,6 +4,11 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Square(BaseGeometry):
+    """Creating a Square class
+
+    Args:
+        BaseGeometry (base class): Parent class
+    """
     def __init__(self, size):
         BaseGeometry.integer_validator(self, 'size', size)
         self.__size = size
