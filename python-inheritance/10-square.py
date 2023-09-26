5fa45009e0ea3abe2270164ffeb1f7b2809cfd69
kier-ious <kier.diemer@gmail.com>
2023-09-26 09:47:37 -0700

    task 10, square class

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""subclass of BaseGeometry, Square"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    def __init__(self, size):
+        BaseGeometry.integer_validator(self, 'size', size)
+        self.__size = size
+        size >= 0
+
+    def area(self):
+        return self.__size
+
+    def __str__(self):
+
+        print("[Square] {}".format(self.__size))
