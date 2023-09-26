97bae6f544a3ee0b163d1593f474baab4d4b6552
kier-ious <kier.diemer@gmail.com>
2023-09-26 10:36:50 -0700

    Fixed the pycodestyle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """subclass of BaseGeometry, Square"""
+
+
 Rectangle = __import__('9-base_geometry').Rectangle
 
 
 class Square(Rectangle):
     """Creating a Square class
-
     Args:
         BaseGeometry (base class): Parent class
     """
     def __init__(self, size):
         """Intitializing new Square class
-
         Args:
             size (Int): Size of Square class
         """
