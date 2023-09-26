2c6ef65145bc6d84fa8cc0485765a99d01320d9a
kier-ious <kier.diemer@gmail.com>
2023-09-26 10:39:03 -0700

    cleaned up blank lines

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """subclass of BaseGeometry, Square"""
-
-
 Rectangle = __import__('9-base_geometry').Rectangle
 
 
 class Square(Rectangle):
-    """Creating a Square class
-    Args:
-        BaseGeometry (base class): Parent class
-    """
+    """Creating a Square class"""
+
     def __init__(self, size):
         """Intitializing new Square class
         Args:
             size (Int): Size of Square class
         """
         self.integer_validator("size", size)
-
         super().__init__(size, size)
         self.__size = size
