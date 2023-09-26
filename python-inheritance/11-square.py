dc1a2a500fafa49087d77503178ada6a7747a7ec
kier-ious <kier.diemer@gmail.com>
2023-09-26 11:53:09 -0700

    task 11, Square #2

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""subclass of BaseGeometry, Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Creating a Square class"""
+
+    def __init__(self, size):
+        """Intitializing new Square class
+        Args:
+            size (Int): Size of Square class
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
