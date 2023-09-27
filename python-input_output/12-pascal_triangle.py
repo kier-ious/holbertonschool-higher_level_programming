827ef853fd8b86412e798b81ec46d84949b048fc
kier-ious <kier.diemer@gmail.com>
2023-09-26 21:07:19 -0700

    fixin pycode

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -13,7 +13,7 @@ def pascal_triangle(n):
         tri = triangles[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i +1])
+            tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
         triangles.append(tmp)
     return triangles
