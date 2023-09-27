f80066b0fa1e593caa95889c08092505997112f0
kier-ious <kier.diemer@gmail.com>
2023-09-26 18:27:49 -0700

    task JSON Derulo

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""""""
+"""Append a file"""
 
 
-def write_file(filename="", text=""):
+def append_file(filename="", text=""):
     """write str to txt file, returns #"""
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Returning JSON format of an object"""
+import json
+
+
+def to_json_string(my_obj):
+    """Method that returns JSON"""
+    return json.dumps(my_obj)
