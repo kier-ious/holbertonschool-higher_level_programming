ed6cf8fe478ef286819ae747db31c0add67b49f3
kier-ious <kier.diemer@gmail.com>
2023-09-26 20:43:14 -0700

    task 10, class filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Creating a Student class"""
+
+
+class Student:
+    """Class for Student instances"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing methods attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method that returs dict info"""
+        if attrs is None:
+            return self.__dict__.copy()
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr\
+                    (self, attr)}
