bc1a1c1c1511c238bc9ee18889798f2249799ae7
kier-ious <kier.diemer@gmail.com>
2023-09-26 20:11:59 -0700

    task 8, mod that returns the dict into a simple data struct

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Module that returns the dictionary info w/ simple data
+structure"""
+
+
+def class_to_json(obj):
+    """Convert object into a JSON dict"""
+    result = {}
+
+    if hasattr(obj, "__dict__"):
+        """Check if the object has a dict attribute"""
+        result = obj.__dict__.copy()
+
+    return result
