983f771ba65412dd84bde5f9e20ad259a6595737
kier-ious <kier.diemer@gmail.com>
2023-09-26 20:31:46 -0700

    Forgot the = in the first instance

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,11 +7,10 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initializing methods attributes"""
-
-        self.first_name first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age =age
 
     def to_json(self):
         """Method that returs dict info"""
-        return self.__dict__
+        return self.__dict__.copy()
