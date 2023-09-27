45a8d1bf91a45c64b6e6da72b3f5e22851b91659
kier-ious <kier.diemer@gmail.com>
2023-09-26 18:17:35 -0700

    task 2, append

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""""""
+
+
+def write_file(filename="", text=""):
+    """write str to txt file, returns #"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
