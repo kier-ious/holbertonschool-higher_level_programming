013198bac920ef849a2a8b43ad5c8ca0e88e6ab3
kier-ious <kier.diemer@gmail.com>
2023-09-26 18:44:55 -0700

    task 5, saving

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -4,5 +4,5 @@ import json
 
 
 def from_json_string(my_str):
-    """Method that returns JSON"""
+    """Method that returns object from JSON"""
     return json.loads(my_str)
--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Save dat object to a file!"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Func from obj to txt in JSON"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
