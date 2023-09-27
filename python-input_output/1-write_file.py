f553e0c24ffc78474c053f95b1e383459d9f162b
kier-ious <kier.diemer@gmail.com>
2023-09-26 18:08:38 -0700

    task 1, return write

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Write a func that writes a str to txt and returns # of chars"""
+
+
+def write_file(filename="", text=""):
+    """Reads txt file, prints to stdout"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
