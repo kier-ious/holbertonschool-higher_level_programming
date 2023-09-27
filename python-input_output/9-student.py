f7fcbcda76c37bb23cc55039aca17bc62eb630ad
kier-ious <kier.diemer@gmail.com>
2023-09-26 20:29:22 -0700

    added a line before instances and removed copy()

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,10 +7,11 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initializing methods attributes"""
+
         self.first_name first_name
         self.last_name = last_name
         self.age =age
 
     def to_json(self):
         """Method that returs dict info"""
-        return self.__dict__.copy()
+        return self.__dict__
