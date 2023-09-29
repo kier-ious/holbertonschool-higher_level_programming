612f968430e6358c7f325475d2e849466177efe8
kier-ious <kier.diemer@gmail.com>
2023-09-29 11:11:29 -0700

    task 2 getters and setters

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+import json
+from models.base import Base
+
+
+
+class Rectangle(Base):
+    """Defining a Rectangle class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize new class"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Getter for width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width in method"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height in method"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate are of the Rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Printing the Rectangle with #"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            rectangle_str = ""
+        for _ in range(self.height):
+            rectangle_str += "#" * self.width + "\n"
+        return rectangle_str.rstrip()
+
+    def __repr__(self):
+        """Return str repr for recreating rectangle"""
+        return f"Rectangle({self.width}, {self.height})"
