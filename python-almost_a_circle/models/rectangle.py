b17b1cc0a32e12772af6abb05e4f67e7de3ed7f3
kier-ious <kier.diemer@gmail.com>
2023-09-29 14:00:14 -0700

    added custom errors

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,30 @@ from dataclasses import dataclass
 class Rectangle(Base):
     """Defining a Rectangle class"""
 
-    width: int
-    height: int
-    x: int = 0
-    y: int = 0
+    __width: int
+    __height: int
+    __x: int = 0
+    __y: int = 0
 
+    def __post_init__(self):
+        """Custom error messages"""
+        if not isinstance(self.width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(self.width, int):
+            raise TypeError("height must be an integer")
+        if not isinstance(self.width, int):
+            raise TypeError("x must be an integer")
+        if not isinstance(self.width, int):
+            raise TypeError("y must be an integer")
+
+        if self.__width < 0:
+            raise ValueError("width must be >= 0")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
+        if self.__x < 0:
+            raise ValueError("x must be >= 0")
+        if self.__y < 0:
+            raise ValueError("y must be >= 0")
 
     def area(self):
         """Calculate are of the Rectangle"""
@@ -19,10 +38,10 @@ class Rectangle(Base):
 
     def display(self):
         """Print Rectangle using the '#' character"""
-        for _ in range(self.y):
+        for _ in range(self.__y):
             print()
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Printing the Rectangle with #"""
