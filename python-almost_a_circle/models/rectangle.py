9ba09761c501bfbc0d40edcf07cb19db323a1c19
kier-ious <kier.diemer@gmail.com>
2023-10-01 12:59:06 -0700

    .

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,7 +95,7 @@ class Rectangle(Base):
     def __str__(self):
         """Printing the Rectangle with #"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y self.width, self.height)
+            self.__id, self.__x, self.__y self.__width, self.__height)
 
 # Update ----------------------------------------------------------------------
     def update(self, *args, **kwargs):
