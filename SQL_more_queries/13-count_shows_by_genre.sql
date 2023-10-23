d67fe3869528e08ed9c4c48377bc255f9560c8bd
kier-ious <kier.diemer@gmail.com>
2023-10-23 11:00:22 -0700

    took out second LEFT JOIN clause

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,6 @@
 SELECT tv_genres.genre AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY tv_genres.genre
-HAVING COUNT(tv_shows.id) > 0
+GROUP BY tv_genres.id
+HAVING COUNT(tv_shows_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
