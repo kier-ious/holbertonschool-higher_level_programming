cf1b73be927137563f7db28acaeeb2df45fbc23c
kier-ious <kier.diemer@gmail.com>
2023-10-23 11:23:12 -0700

    fixed over all query for tv_show_genres

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Script that lists all genres from database and shows # of shows linked to it
-SELECT tv_genres.name AS genre, COUNT(tv_shows_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_shows_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
-HAVING COUNT(tv_shows_genres.show_id) > 0
+HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
