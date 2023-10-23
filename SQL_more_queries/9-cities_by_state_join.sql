295ddf5da0bb1b58150ea410b30532a768ee10ea
kier-ious <kier.diemer@gmail.com>
2023-10-22 21:00:37 -0700

    put back ASC an dremoved INNER from JOIN

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 --Script that lists all citites CONTAINED in the database
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.states_id = states_id
-ORDER BY cities.id;
+JOIN states ON cities.states_id = states_id
+ORDER BY cities.id ASC;
