6b81d5fd92642826c04bde3b226cfdcabbce3091
kier-ious <kier.diemer@gmail.com>
2023-10-22 21:18:02 -0700

    added filter condition that limits results

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,9 @@
 -- Script that lists all tv shows in database that are in the same genre
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.id IN (
+  SELECT DISTINCT show_id
+  FROM tv_show_genres
+)
 ORDER BY tv_show.title, tv_show_genres.genre_id ASC;
