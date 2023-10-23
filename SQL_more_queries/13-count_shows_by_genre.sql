8f90f654168feb42aaaec6bd1f0fcb7e164386a4
kier-ious <kier.diemer@gmail.com>
2023-10-23 10:56:20 -0700

    corrected typo and added comma in SELECT clause

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Script that lists all genres from database and shows # of shows linked to it
-SELECT tv_genres.genre AS genre COUNT(tv_shows.id) AS number_of_shows
+SELECT tv_genres.genre AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
