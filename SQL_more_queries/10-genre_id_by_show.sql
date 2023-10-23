e4c9b7ba38059179b27ae002f72317eea3204632
kier-ious <kier.diemer@gmail.com>
2023-10-22 21:21:56 -0700

    took out the INNER in the JOIN condition

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- Script that lists all tv shows in database that are in the same genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.id IN (
   SELECT DISTINCT show_id
   FROM tv_show_genres
