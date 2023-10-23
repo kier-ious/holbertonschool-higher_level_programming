1435baadf6f0c3fb32021fcd3a88e9e897b1eeed
kier-ious <kier.diemer@gmail.com>
2023-10-23 12:33:07 -0700

    ""

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,5 @@
 -- Script that lists all shows, and all genres linked to that show, from the db
-SELECT tv_show_title AS show_title IFNULL(tv_genres.name, 'NULL') AS genre_name
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+SELECT title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY show_title ASC, genre_name ASC;
+ORDER BY title, tv_genres.name ASC;
