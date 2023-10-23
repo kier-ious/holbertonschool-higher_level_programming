cdbabfe2b9ed6dd6763237f3fb5ae4f53b23931a
kier-ious <kier.diemer@gmail.com>
2023-10-22 20:10:29 -0700

    corrected my typo in the database name

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Script that creates database and table
-CREATE DATABASE IF NOT EXISTS hbtn_04_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-USE hbtn_04_usa;
+USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states (
   id INT NOT NULL AUTO_INCREMENT,
