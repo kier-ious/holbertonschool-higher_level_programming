e2cc9b06d2d65375a2320f7acba72c6d98b1ea1d
kier-ious <kier.diemer@gmail.com>
2023-10-22 21:01:47 -0700

    forgot the space after the -- in the comment line

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
---Script that lists all citites CONTAINED in the database
+-- Script that lists all citites CONTAINED in the database
 SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.states_id = states_id
