dace0b6fdae8b4548101dfd1bae96e79bc24c859
kier-ious <kier.diemer@gmail.com>
2023-10-23 11:19:37 -0700

    ""

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name AS genre, COUNT(tv_shows_genres.show_id) AS number_of_show
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
-HAVING COUNT(tv_shows_genres.show_id)>0
+HAVING COUNT(tv_shows_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
