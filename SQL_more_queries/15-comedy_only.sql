dec8816c9a3082d19e2b2f753c56c16fb2f2402d
kier-ious <kier.diemer@gmail.com>
2023-10-23 11:43:35 -0700

    Changed SELECT clause to reflect genre

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Script that lists all the Comedy shows in the db
-SELECT tv_shows.title
+SELECT tv_shows.genre
 FROM tv_shows
 JOIN tv_genres ON tv_shows.id = tv_genres.show_id
 WHERE tv_genres.name = 'Comedy'
