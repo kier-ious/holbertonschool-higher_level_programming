6018a667deb992904714db69130a264117ea327d
kier-ious <kier.diemer@gmail.com>
2023-10-22 20:07:20 -0700

    added spacing

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- Script that creates database and table
 CREATE DATABASE IF NOT EXISTS hbtn_04_usa;
+
 USE hbtn_04_usa;
+
 CREATE TABLE IF NOT EXISTS states (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(256) NOT NULL
