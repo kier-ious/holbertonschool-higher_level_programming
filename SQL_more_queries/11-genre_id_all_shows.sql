a42b230e48d258afd27fdaf34839c0bebdd57c40
kier-ious <kier.diemer@gmail.com>
2023-10-23 09:01:16 -0700

    Changed joining promt to by RIGHT JOIN

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- List all the shows that are contained in the hbtn_0d_tvshows database
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
