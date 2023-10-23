b46e835d47277bc68ec83981e80697cf5379e170
kier-ious <kier.diemer@gmail.com>
2023-10-22 21:22:37 -0700

    took out the conditonal statement

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,8 +2,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.id IN (
-  SELECT DISTINCT show_id
-  FROM tv_show_genres
-)
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
