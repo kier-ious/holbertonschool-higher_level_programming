7698f6ce20cd6feaf8cab5472b447f52521329ba
kier-ious <kier.diemer@gmail.com>
2023-10-24 10:00:59 -0700

    ""

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -24,9 +24,9 @@ def list_states(username, password, database_name, host, port):
 	db.close()
 
 if __name__ == "__main__":
-	host="localhost",
 	username=sys.argv[1],
 	password=sys.argv[2],
 	database_name=sys.argv[3],
-	port=3306
+	host = "localhost",
+	port = 3306
 	list_states(username, password, database_name, host, port)
