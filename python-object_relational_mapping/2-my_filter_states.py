9c74121761219d2f4ce1f126dccfe48343b4e3a2
kier-ious <kier.diemer@gmail.com>
2023-10-24 13:02:43 -0700

    list states search that match arg

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Displays arg and all values in states from db where name matches arg """
+
+
+import MySQLdb
+import sys
+
+
+def list_states_searched(username, password, database_name, state_searched):
+
+    """ Listing states in db """
+    db = MySQLdb.connect(
+        user=username,
+        passwd=password,
+        db=database_name,
+        host='localhost',
+        port=3306
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE \
+        '{}' ORDER BY states.id".format(sys.argv[4]))
+
+    results = cursor.fetchall()
+
+    for state in results:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    host = 'localhost'
+    port = 3306
+    list_states_searched(username, password, database_name, host, port)
