b0a734436516c555432a198f97a67392b828e958
kier-ious <kier.diemer@gmail.com>
2023-10-24 13:59:41 -0700

    ""

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -36,6 +36,4 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
-    host = 'localhost'
-    port = 3306
     list_cities(username, password, database_name)
