e02e2cf490f3d98c81ed05ed7e436782379c7f69
kier-ious <kier.diemer@gmail.com>
2023-10-24 14:12:06 -0700

    changed the JOIN tool to LEFT JOIN

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,7 @@ def list_cities(username, password, database_name):
     cursor = db.cursor()
     cursor.execute("""
         SELECT cities.id, cities.name, state.name
-        FROM cities JOIN states ON cities.state_id = states.id
+        FROM cities LEFT JOIN states ON cities.state_id = states.id
         ORDER BY cities.id
     """)
 
