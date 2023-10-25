33ed4f5629487dcb91acc68aaaa646966e112bbd
kier-ious <kier.diemer@gmail.com>
2023-10-24 17:35:00 -0700

    Script that prints the state obj w/ the name passed as arg fro db

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Lists all states from db hbtn_0e_0_usa """
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def list_states(username, password, database_name, state_name):
+    """ Listing states in db """
+    engine = create_engine(
+        f'mysql://{username}:{password}@localhost:3306/{database_name}'
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state is not None:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+    list_states(username, password, database_name, state_name)
