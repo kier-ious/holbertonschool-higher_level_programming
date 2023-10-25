4e6985dc7d7c1bc0d1e5b0ddb23e4188f807d713
kier-ious <kier.diemer@gmail.com>
2023-10-24 18:29:30 -0700

    fixed the models_city file with attrs

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -12,7 +12,8 @@ Base = declarative_base()
 class City(Base):
     """ Base class: State"""
     __tablename__ = "cities"
+
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     state = relationship("State")
