61a2449f715ba6b902010f6974b0407fbf0c5066
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-24 11:10:11 -0600

    task5 using a unary operator to attempt to convert 1st arg to a #

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const firstArgument = args[0];
+const convertedNumber = +firstArgument;
+
+if (Number.isFinite(convertedNumber) && Number.isInteger(convertedNumber)) {
+  console.log('My number: ${convertedNumber}');
+} else {
+  console.log("Not a number");
+}
