07018544daa630d2fe7e1401a03141316c6f4291
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-24 15:19:56 -0600

    fixed semistandard and consolidated code into a more simple easy to read version

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
+const arg = process.argv[2];
 
 function factorial (n) {
-  // base case, factorial of 0 is 1
-  if (isNaN(n) || n < 0) {
+  if (n === 0 || n === 1) {
     return 1;
+  } else {
+    return n * factorial(n - 1);
   }
-  // recursive case, n! = n* (n-1)!
-  return n * factorial(n - 1);
 }
-
-const args = process.argv.slice(2);
-const input = parseInt(args[0]);
-
-if (!isNaN(input)) {
-  console.log(factorial(input));
-} else {
-  console.log('Infinity');
-}
+if (parseInt(arg)) {
+  console.log(factorial(arg));
+} else console.log(1);
