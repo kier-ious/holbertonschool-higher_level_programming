117c420124ae987908a41c1536e976aa03329710
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-24 11:28:53 -0600

    forgot to capitalize the 'S' in JavaScript

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let y = 0; y < lines.length; y++) {
   console.log(lines[y]);
 }
