7615981953c8cc146135837a0ccfe7f6451804d7
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-26 19:58:15 -0600

    tried alternate route using tostring method

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return function convertNumber (number) {
-    if (number === 0) {
-      return '';
-    } else {
-      return convertNumber(Math.floor(number / base)) + (number % base);
-    }
+  return function (number) {
+      return number.toString(base);
   };
 };
