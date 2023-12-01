114f79c9b3731fe28ba84ce854e63737b98fb301
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-12-01 12:31:27 -0600

    ran semistandard on file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if(err)console.error(err);
+  if (err)console.error(err);
   else console.log(data);
 });
