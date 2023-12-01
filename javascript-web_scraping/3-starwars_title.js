11fddcc7c05881364eabcb4e08e2e5394877da6a
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-12-01 13:55:16 -0600

    task3, script that writes the title of a Star Wars movie

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
+const request = require('request');
+const moveId = process.argv[2];
+if (!moveId) {
+  console.error('Error');
+  process.exit(1);
+}
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${moveId}`;
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error.message);
+  } else {
+    try {
+      const movieData = JSON.parse(body);
+      console.log(`${movieData.title}`);
+    } catch (parseError) {
+      console.error('Error:', parseError.message);
+    }
+  }
+});
