1df64e9228e124bda51b741589ff05ac97b7d543
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-12-01 14:56:10 -0600

    task6, script that computes the # of tasks completed by user ID

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+const request = require('request');
+const aprUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+// makes request to the API Url
+request(aprUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+  // parsin' the response to the body
+  const todos = JSON.parse(body);
+
+  // create a map to store # of completed tasks per user
+  const completedTasksByUser = new Map();
+
+  // iterate through tasks
+  todos.forEach((task) => {
+    if (task.completed) {
+      completedTasksByUser.set(
+        task.userId,
+        (completedTasksByUser.get(task.userId) || 0) + 1
+      );
+    }
+  });
+  // print dem results!
+  completedTasksByUser.forEach((count, userId) => {
+    console.log(`'${userId}': ${count},`);
+  });
+});
