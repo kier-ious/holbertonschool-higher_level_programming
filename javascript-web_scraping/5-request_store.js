96bb04f4af8e08d1d9077823b408635e7a0cca19
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-12-01 14:26:26 -0600

    task5, script that gets the contents of a webpage and stores it in a file.

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,25 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Error:');
+  process.exit(1);
+}
+// makes a request to the choosen url
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+  // wright the body response to selected file
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error:', err);
+      process.exit(1);
+    }
+    console.log(`Contents of ${url} saved to ${filePath}`);
+  });
+});
