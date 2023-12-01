819a695a4534d59e10069bcc3c1571c7cb1edfd1
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-12-01 13:03:26 -0600

    task2, script that displays the status of a GET request.

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+if (!url) {
+  console.error(err);
+  process.exit(1);
+}
+request.get(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error.message);
+  } else {
+    console.log(`code:${response.statusCode}`);
+  }
+});
