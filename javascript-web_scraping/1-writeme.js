994abc43270272519ace2d2a93d831835d6175dd
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-12-01 12:42:44 -0600

    task1, script that writes a str to a f

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
+  if (err)console.error(err);
+  else console.log(data);
+});
