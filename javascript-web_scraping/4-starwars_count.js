383cebb3deb1484636f8a7dbd7dd4f60c7921b61
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-12-03 16:17:09 -0600

    changed the error message and took out the jinja f strings to be printed

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,26 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrl = process.argv[2];
+
 if (!apiUrl) {
-  console.error('Error', error.message);
+  console.error('Error: error.message');
   process.exit(1);
 }
 const characterId = '18';
+
 // makes request to SW API
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     process.exit(1);
+  } else {
+    // parse the JSON response
+    const data = JSON.parse(body);
+
+    // filter movies where Wedge Salad is present
+    const moviesWithWedge = data.results.filter(movie =>
+      movie.characters.includes(`${characterId}`));
+      // print the # of movies w/ Wedge Salad
+    console.log(moviesWithWedge.length);
   }
-  // parse the JSON response
-  const data = JSON.parse(body);
-  // filter movies where Wedge Salad is present
-  const moviesWithWedge = data.results.filter(movie =>
-    movie.characters.includes(`${characterId}`));
-    // print the # of movies w/ Wedge Salad
-  console.log(`${moviesWithWedge.length}`);
 });
