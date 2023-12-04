1768be40def8d8f5ca024f9483d31dc2439035b6
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-12-04 17:53:44 -0600

    task3, script that toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header:

--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -1,7 +1,5 @@
 //  Adds the class 'red' to <header> tag when user clicks specified tag
-$(function () {
-  $('#red_header').click(function () {
-    // Adding 'red' class to <header>
-    $('header').addClass('red');
-  });
+$('#red_header').click(function () {
+  // Adding 'red' class to <header>
+  $('header').addClass('red');
 });
